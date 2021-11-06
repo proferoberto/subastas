d2e9e6a94e06eff8fdf0ce588a1862b527705d2a
Ubuntu <ubuntu@ip-172-31-37-175.us-east-2.compute.internal>
2021-11-06 01:49:59 +0000

    teminacion de la caracteristica inicio de session con google.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :google_oauth2, '590979775549-g0kgq30il7gqrgo5fvrrr5tv85pjkfts.apps.googleusercontent.com', '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'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   root 'auctions#index'
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  #devise_for :users
   
   resources :auctions
   resources :users do
